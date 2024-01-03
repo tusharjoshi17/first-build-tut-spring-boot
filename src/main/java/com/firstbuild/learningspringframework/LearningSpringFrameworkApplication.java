df59110cf0e6ab649623327c945ddbff2c62fcf2
Tushar Joshi <tusharjoshi52@gmail.com>
2024-01-04 04:29:26 +0530

    first commit

---
+++ src/main/java/com/firstbuild/learningspringframework/LearningSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.firstbuild.learningspringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstbuild/learningspringframework/LearningSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstbuild.learningspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
