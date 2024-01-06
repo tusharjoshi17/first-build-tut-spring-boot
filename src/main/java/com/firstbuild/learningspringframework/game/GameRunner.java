27f91f39742d76fa3179a437e41ad621fa51f181
Tushar Joshi <tusharjoshi52@gmail.com>
2024-01-07 00:58:27 +0530

    Tightly Coupled concept completed

--- src/main/java/com/firstbuild/learningspringframework/game/GameRunner.java
+++ src/main/java/com/firstbuild/learningspringframework/game/GameRunner.java
@@ -2,15 +2,16 @@ package com.firstbuild.learningspringframework.game;
 
 public class GameRunner {
 	
-	MarioGame game;
+//	MarioGame game;
+	private SuperContraGame game;
 
-	public GameRunner(MarioGame game) {
+	public GameRunner(SuperContraGame game) {
 		this.game = game;
 	}
 
 	public void run() {
 		// TODO Auto-generated method stub
-		System.out.println("Running game: "+game);
+		System.out.println("Running game: "+ game);
 		
 		game.up();
 		game.down();
---
+++ src/main/java/com/firstbuild/learningspringframework/game/SuperContraGame.java
@@ -0,0 +1,20 @@
+package com.firstbuild.learningspringframework.game;
+
+public class SuperContraGame {
+	
+	public void up(){
+		System.out.println("Jump");	
+		}
+	
+	public void down() {
+		System.out.println("Crouch");
+	}
+	
+	public void left() {
+		System.out.println("Move backwards");
+	}
+	
+	public void right() {
+		System.out.println("Fire");
+	}
+}
